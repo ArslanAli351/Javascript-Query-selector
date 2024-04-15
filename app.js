6f5fbb2d50aed6abc5bb151e1363b8562a6e3802
Arslan <muhammadarslanrajput12@gmail.com>
2024-04-15 10:10:05 -0700

    javascript

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+// let lable = document.querySelectorAll("#content");
+// console.log(lable);
+// console.log(lable[0].innerHTML);
+// lable[0].innerHTML ="<i>Hello Mass</i>"
+
+
+
+let imgElement= document.querySelectorAll("img");
+console.log(imgElement);
+imgElement[0].src="download (2).jpg"
+imgElement[0].className="contantcontaner"
+
+
+
+
+let samander = document.querySelectorAll("img");
+console.log(samander);
+samander[1] .className ="samander"
+
+
+let aElement=document.querySelectorAll("a");
+console.log(aElement);
+aElement[0].href="https://github.com/arslanai"
+aElement[0].innerHTML="github"
+aElement[0].className="aElement"
